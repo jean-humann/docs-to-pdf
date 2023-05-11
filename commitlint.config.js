567f5ad4b804e1077870d99dc101eea18bbfeb2a
Jean Humann <76565654+jean-humann@users.noreply.github.com>
2023-05-11 19:18:21 +0200

    chore: add husky and commitlint (#12)

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+// See https://github.com/conventional-changelog/commitlint/blob/master/docs/reference-rules.md
+module.exports = {
+    extends: ['@commitlint/config-conventional'],
+    rules: {
+      // Override. The subject may be the name of a class.
+      'subject-case': [0],
+      // Override. Most UIs wrap the body.
+      'body-max-line-length': [0],
+      // Override. Most UIs wrap the footer.
+      'footer-max-line-length': [0],
+    },
+  };
