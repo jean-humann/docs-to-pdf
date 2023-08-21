de3f3713ff2ea1ed6722ccec39c37a03249f2822
Jean Humann <jean.humann@gmail.com>
2023-08-21 14:03:15 +0200

    feat: add docusaurus command

--- src/command.ts
+++ src/command.ts
@@ -15,96 +15,24 @@ const version = require('../package.json').version;
 console_stamp(console);
 
 export function makeProgram() {
-  const program = new Command();
-  program
-    .name('docs-to-pdf')
+  const program = new Command('');
+  const docstopdf = program
+    .command('docs-to-pdf')
     .version(version, '-v, --vers', 'output the current version')
     .showSuggestionAfterError()
     .configureHelp({
       sortSubcommands: true,
       sortOptions: true,
-    })
-    .requiredOption(
-      '--initialDocURLs <urls>',
-      'set urls to start generating PDF from',
-      commaSeparatedList,
-    )
-    .option(
-      '--excludeURLs <urls>',
-      'urls to be excluded in PDF',
-      commaSeparatedList,
-    )
-    .option(
-      '--contentSelector <selector>',
-      'used to find the part of main content',
-    )
-    .option('--paginationSelector <selector>', 'used to find next url')
-    .option(
-      '--excludeSelectors <selectors>',
-      'exclude selector ex: .nav',
-      commaSeparatedList,
-    )
-    .option(
-      '--cssStyle <cssString>',
-      'css style to adjust PDF output ex: body{padding-top: 0;}',
-    )
-    .option('--outputPDFFilename <filename>', 'name of output PDF file')
-    .option(
-      '--pdfMargin <margin>',
-      'set margin around PDF file',
-      generatePuppeteerPDFMargin,
-    )
-    .option('--pdfFormat <format>', '(DEPRECATED use paperFormat)') //TODO: Remove at next major version, replaced by paperFormat
-    .option('--paperFormat <format>', 'pdf format ex: A3, A4...')
-    .option('--coverTitle <title>', 'title for PDF cover')
-    .option(
-      '--coverImage <src>',
-      'image for PDF cover. *.svg file not working!',
-    )
-    .option('--disableTOC', 'disable table of contents')
-    .option('--coverSub <subtitle>', 'subtitle for PDF cover')
-    .option(
-      '--waitForRender <timeout>',
-      'wait for document render in milliseconds',
-    )
-    .option('--headerTemplate <html>', 'html template for page header')
-    .option('--footerTemplate <html>', 'html template for page footer')
-    .option(
-      '--puppeteerArgs <selectors>',
-      'add puppeteer arguments ex: --sandbox',
-      commaSeparatedList,
-    )
-    .option(
-      '--protocolTimeout <timeout>',
-      'timeout setting for individual protocol calls in milliseconds',
-      commaSeparatedList,
-    )
-    .option('--filterKeyword <filterKeyword>', 'meta keyword to filter pages')
-    .option(
-      '--baseUrl <baseUrl>',
-      'base URL for all relative URLs. Allows to render the pdf on localhost while referencing the deployed page.',
-    )
-    .option(
-      '--excludePaths <paths>',
-      'paths to be excluded in PDF',
-      commaSeparatedList,
-    )
-    .option(
-      '--restrictPaths',
-      'only the paths in the --initialDocURLs will be included in the PDF',
-    )
-    .option(
-      '--openDetail',
-      'open details elements in the PDF, default is open',
-    );
+    });
 
-  program
-    .command('docusaurus', { isDefault: true })
+  docstopdf
+    .command('docusaurus')
     .alias('d')
     .description('generate PDF from Docusaurus site')
     .option(
       '--version <version>',
       'version of Docusaurus site to generate PDF from',
+      '2',
     )
     .option(
       '--docsDir <dir>',
@@ -112,6 +40,7 @@ export function makeProgram() {
     )
     .action((options: DocusaurusOptions) => {
       console.debug('Generate from Docusaurus');
+      console.debug(options);
       generateDocusaurusPDF(options)
         .then(() => {
           console.log(chalk.green('Finish generating PDF!'));
@@ -123,8 +52,8 @@ export function makeProgram() {
         });
     });
 
-  program
-    .command('core')
+  docstopdf
+    .command('core', { isDefault: true })
     .description("generate PDF from Core's options")
     .action((options: GeneratePDFOptions) => {
       if (options.pdfFormat) {
@@ -142,6 +71,83 @@ export function makeProgram() {
           process.exit(1);
         });
     });
+
+  docstopdf.commands.forEach((cmd) => {
+    cmd
+      .requiredOption(
+        '--initialDocURLs <urls>',
+        'set urls to start generating PDF from',
+        commaSeparatedList,
+      )
+      .option(
+        '--excludeURLs <urls>',
+        'urls to be excluded in PDF',
+        commaSeparatedList,
+      )
+      .option(
+        '--contentSelector <selector>',
+        'used to find the part of main content',
+      )
+      .option('--paginationSelector <selector>', 'used to find next url')
+      .option(
+        '--excludeSelectors <selectors>',
+        'exclude selector ex: .nav',
+        commaSeparatedList,
+      )
+      .option(
+        '--cssStyle <cssString>',
+        'css style to adjust PDF output ex: body{padding-top: 0;}',
+      )
+      .option('--outputPDFFilename <filename>', 'name of output PDF file')
+      .option(
+        '--pdfMargin <margin>',
+        'set margin around PDF file',
+        generatePuppeteerPDFMargin,
+      )
+      .option('--pdfFormat <format>', '(DEPRECATED use paperFormat)') //TODO: Remove at next major version, replaced by paperFormat
+      .option('--paperFormat <format>', 'pdf format ex: A3, A4...')
+      .option('--coverTitle <title>', 'title for PDF cover')
+      .option(
+        '--coverImage <src>',
+        'image for PDF cover. *.svg file not working!',
+      )
+      .option('--disableTOC', 'disable table of contents')
+      .option('--coverSub <subtitle>', 'subtitle for PDF cover')
+      .option(
+        '--waitForRender <timeout>',
+        'wait for document render in milliseconds',
+      )
+      .option('--headerTemplate <html>', 'html template for page header')
+      .option('--footerTemplate <html>', 'html template for page footer')
+      .option(
+        '--puppeteerArgs <selectors>',
+        'add puppeteer arguments ex: --sandbox',
+        commaSeparatedList,
+      )
+      .option(
+        '--protocolTimeout <timeout>',
+        'timeout setting for individual protocol calls in milliseconds',
+        commaSeparatedList,
+      )
+      .option('--filterKeyword <filterKeyword>', 'meta keyword to filter pages')
+      .option(
+        '--baseUrl <baseUrl>',
+        'base URL for all relative URLs. Allows to render the pdf on localhost while referencing the deployed page.',
+      )
+      .option(
+        '--excludePaths <paths>',
+        'paths to be excluded in PDF',
+        commaSeparatedList,
+      )
+      .option(
+        '--restrictPaths',
+        'only the paths in the --initialDocURLs will be included in the PDF',
+      )
+      .option(
+        '--openDetail',
+        'open details elements in the PDF, default is open',
+      );
+  });
 
   return program;
 }
--- src/provider/docusaurus.ts
+++ src/provider/docusaurus.ts
@@ -12,12 +12,41 @@ export async function generateDocusaurusPDF(
   console.log(`Docusaurus version: ${version}`);
   console.log(`Docs directory: ${docsDir}`);
   core.contentSelector = 'article';
-  core.paginationSelector = 'a.pagination-nav__link.pagination-nav__link--next';
-  core.excludeSelectors = [
-    '.margin-vert--xl a',
-    "[class^='tocCollapsible']",
-    '.breadcrumbs',
-    '.theme-edit-this-page',
-  ];
+  
+
+
+  // Pagination and exclude selectors are different depending on Docusaurus version
+  if (version == 2) {
+      console.debug('Docusaurus version 2');
+      core.paginationSelector =
+        'a.pagination-nav__link.pagination-nav__link--next';
+      core.excludeSelectors.push
+      core.excludeSelectors = [
+        '.margin-vert--xl a',
+        "[class^='tocCollapsible']",
+        '.breadcrumbs',
+        '.theme-edit-this-page',
+      ];
+  }
+  else if (version == 1) {
+      console.debug('Docusaurus version 1');
+      core.paginationSelector = '.docs-prevnext > a.docs-next';
+      core.excludeSelectors = [
+        '.fixedHeaderContainer',
+        'footer.nav-footer',
+        '#docsNav',
+        'nav.onPageNav',
+        'a.edit-page-link',
+        'div.docs-prevnext',
+      ];
+      core.cssStyle = `
+      .navPusher {padding-top: 0;}
+      `;
+  }
+  else {
+      console.error(`Unsupported Docusaurus version: ${version}`);
+      process.exit(1); 
+  }
+  console.debug(core);
   await generatePDF(core);
 }
